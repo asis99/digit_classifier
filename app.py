eec7ba77a85f6fe8390d9be7dc6990278ab73261
asis.d <asis.d@apollodart.com>
2024-08-23 10:35:59 +0530

    added exception to cath the error codes

--- app.py
+++ app.py
@@ -1,66 +1,55 @@
-# import streamlit as st
-# from model.makeinference import MakePredictions
-# import sys
-
-
-# sideBar=st.sidebar # creating a side bar component
-# inputImage=sideBar.file_uploader(label="Upload a image of your digit", type=["jpg", "png"])
-# st.logo(image="chip-ai-svgrepo-com.svg")
-# try:
-#     if inputImage is not None:
-#         # Open the image using PIL
-#         inputImage = inputImage
-        
-#         col1, col2 = st.columns([1, 2], gap="large")
-        
-#         # with col1:
-#         st.image(inputImage, caption="Uploaded Image", use_column_width=True)
-#         modelOutput = MakePredictions(digitImage=inputImage)
-        
-#         # with col2:
-#         probData=[i for i in modelOutput.get("Probabilities").tolist()[0]]
-#         st.write("okkkk")
-#         st.line_chart(data=probData, x_label="Predictions", y_label="Probability")
-#             # sns.barplot(x=[i for i in range(10)], y=probData)
-
-#         st.write("Model Output:")
-#         st.write(modelOutput.get("modelOutput"))
-    
-#     else:
-#         st.markdown(
-#         """
-#         <h1 style='color: #ff6347;'>AI Powered Digit Recognizer</h1>
-#         """, unsafe_allow_html=True
-#         )
-#         st.divider()
-#         col1, col2 = st.columns(2, gap="large")
-#         with col1:
-#             st.image(image="artificial-intelligence.jpg", width=200)
-#         with col2:
-#             st.subheader("Instruction for image upoading")
-#             st.divider()
-#             st.write('''
-#                     Please upload an image that has been cropped to a 1:1 ratio, ensuring the digit is centered within the frame & background must be white.
-#                     Make sure the digit is written on white paper using any pen or pencil. 
-#                     Note that blurred images may not be accurately recognized by the system. 
-#                     The model may occasionally misinterpret certain digits, so if the prediction is incorrect,
-#                     try to re-crop it, zoom it a little and upload the image. For optimal results, use a high-definition camera to provide clearer 
-#                     images, which will help the system recognize the digit more accurately.
-#                     ''')
-# except Exception as e:
-#     print(f"Exception is {str(e)}")
-#     line_number = sys.exc_info()[-1].tb_lineno
-#     print(f"Error is at line no {line_number}")
-
 import streamlit as st
+from model.makeinference import MakePredictions
+import sys
 import pandas as pd
 
-# Create a simple dataframe
-data = pd.DataFrame({
-    'Categories': ['A', 'B', 'C'],
-    'Values': [10, 20, 15]
-})
 
-# Display the bar chart
-st.bar_chart(data.set_index('Categories'))
+sideBar=st.sidebar # creating a side bar component
+inputImage=sideBar.file_uploader(label="Upload a image of your digit", type=["jpg", "png"])
+st.logo(image="chip-ai-svgrepo-com.svg")
+try:
+    if inputImage is not None:
+        # Open the image using PIL
+        inputImage = inputImage
+        
+        col1, col2 = st.columns([1, 2], gap="large")
+        
+        # with col1:
+        st.image(inputImage, caption="Uploaded Image", use_column_width=True)
+        modelOutput = MakePredictions(digitImage=inputImage)
+        
+        # with col2:
+        probData=[i for i in modelOutput.get("Probabilities").tolist()[0]]
+        data=pd.DataFrame({"x":probData})
+        st.write("okkkk")
+        st.bar_chart(data.set_index('x'))
+            # sns.barplot(x=[i for i in range(10)], y=probData)
 
+        st.write("Model Output:")
+        st.write(modelOutput.get("modelOutput"))
+    
+    else:
+        st.markdown(
+        """
+        <h1 style='color: #ff6347;'>AI Powered Digit Recognizer</h1>
+        """, unsafe_allow_html=True
+        )
+        st.divider()
+        col1, col2 = st.columns(2, gap="large")
+        with col1:
+            st.image(image="artificial-intelligence.jpg", width=200)
+        with col2:
+            st.subheader("Instruction for image upoading")
+            st.divider()
+            st.write('''
+                    Please upload an image that has been cropped to a 1:1 ratio, ensuring the digit is centered within the frame & background must be white.
+                    Make sure the digit is written on white paper using any pen or pencil. 
+                    Note that blurred images may not be accurately recognized by the system. 
+                    The model may occasionally misinterpret certain digits, so if the prediction is incorrect,
+                    try to re-crop it, zoom it a little and upload the image. For optimal results, use a high-definition camera to provide clearer 
+                    images, which will help the system recognize the digit more accurately.
+                    ''')
+except Exception as e:
+    print(f"Exception is {str(e)}")
+    line_number = sys.exc_info()[-1].tb_lineno
+    print(f"Error is at line no {line_number}")
